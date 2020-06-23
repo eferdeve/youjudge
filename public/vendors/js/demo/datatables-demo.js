3bb4fc78297c3efb100ae7657cfe22bc6996533c
Ferres Enzo <Enzo@MacBook-Pro-de-Ferres.local>
2020-06-23 15:49:42 +0200

    pagination / searchbar sbadmin

--- public/vendors/js/demo/datatables-demo.js
+++ public/vendors/js/demo/datatables-demo.js
@@ -1,4 +1,13 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('.dataTable').DataTable({
+    columnDefs: [
+      { "orderable": false, "targets": 3 }
+    ],
+    language: {
+        "url": "https://cdn.datatables.net/plug-ins/1.10.21/i18n/French.json"
+    },
+    ordering: true,
+    responsive: true
+});
 });
